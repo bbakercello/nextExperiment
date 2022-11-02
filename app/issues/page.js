f03657cfa7cb059da45e93beb837ad175f465236
bbakercello <bbakercello@gmail.com>
2022-11-02 09:41:29 -0400

    initial pages created

---
+++ app/issues/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <h1>Issues Page</h1>
+  )
+}
+
+export default page
---
+++ app/page.js
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div>
+    <h1>The Starting Page</h1>
+    </div>
+  )
+}
